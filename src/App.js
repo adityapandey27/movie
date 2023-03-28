af01f6c28ffca3f9b80687564b82702668d77ec7
Aditya Pandey <adityapandey272002@gmail.com>
2023-03-28 16:47:43 +0530

    remaining changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Banner from './Components/Banner';
 import Movies from './Components/Movies';
 import Navbar from './Components/Navbar';
-import Pagination from './Components/Pagination';
+// import Pagination from './Components/Pagination';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Favourites from './Components/Favourites';
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <>
         <Banner/>
         <Movies/>
-        <Pagination/>
+        
         </>
         }></Route>
         
--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
 
-function Pagination() {
+function Pagination({page,goBack,goAhead}) {
   
   return (
     <div className='w-full flex justify-center mb-8'>
@@ -9,16 +7,22 @@ function Pagination() {
         text-indigo-500
         border-r-0
         rounded-l-xl
-        ' onClick={goBack}>Previous</button>
+        ' 
+        onClick={goBack}
+        >Previous</button>
         <button className='p-2 border-2 border-indigo-500
         text-indigo-500
         bg-gray-300
-        '>{page}</button>
+        '>
+          {page}
+        </button>
          <button className='p-2 border-2 border-indigo-500
         text-indigo-500
         border-l-0
         rounded-r-xl
-        'onClick={goAHead}>Next</button>
+        '
+        onClick={goAhead}
+        >Next</button>
     </div>
   )
 }
